c154971014daf63be2d68c7f137e07894536c730
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2025-10-31 09:44:32 -0300

    chore(config): setup jest with vue3-jest transformer

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/App.vue',
+    '!src/stores/remindersStore.js'
+  ],
+    testMatch: [
+    '<rootDir>/src/**/*.spec.js',
+  ],
+};
